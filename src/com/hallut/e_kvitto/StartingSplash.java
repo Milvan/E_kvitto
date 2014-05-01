e83d4dbbe190698859617b28d9678b4990d445c5
Marcus <marcus.larson@live.com>
2014-05-01 18:35:02 +0200

    Login and home

---
+++ src/com/hallut/e_kvitto/StartingSplash.java
@@ -0,0 +1,84 @@
+package com.hallut.e_kvitto;
+
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class StartingSplash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_starting_splash);
+		
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(5000);
+				} catch (InterruptedException e){
+					e.printStackTrace();
+				} finally {
+					Intent openHomeScreen = new Intent("com.hallut.e_kvitto.LOGIN");
+					startActivity(openHomeScreen);
+				}
+			}
+		};
+		timer.start();
+
+//		if (savedInstanceState == null) {
+//			getSupportFragmentManager().beginTransaction()
+//					.add(R.id.container, new PlaceholderFragment()).commit();
+//		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.starting_splash, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_starting_splash,
+					container, false);
+			return rootView;
+		}
+	}
+	
+	@Override
+	public void onPause(){
+		super.onPause();
+		finish();
+	}
+
+}
