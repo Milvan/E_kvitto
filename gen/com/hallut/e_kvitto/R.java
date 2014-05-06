6b98b749eb67a7872895e6cad782f764eb11b491
fredriknorrman <fredriknorrman@192.168.0.196>
2014-05-06 17:18:55 +0200

    fixat lite layout och nya listadaptern, adaptern är dock ej klar, just nu visas bara ett objekt ur den länkade listan som innehåller kvitton

--- gen/com/hallut/e_kvitto/R.java
+++ gen/com/hallut/e_kvitto/R.java
@@ -1078,12 +1078,13 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int authenticate=0x7f020057;
-        public static final int bankkort=0x7f020058;
-        public static final int home1=0x7f020059;
+        public static final int bluebackground=0x7f020058;
+        public static final int home=0x7f020059;
         public static final int ic_launcher=0x7f02005a;
-        public static final int kvitto=0x7f02005b;
-        public static final int kvittokalender=0x7f02005c;
-        public static final int kvitton=0x7f02005d;
+        public static final int login=0x7f02005b;
+        public static final int loginbackground=0x7f02005c;
+        public static final int splash=0x7f02005d;
+        public static final int whitebackground=0x7f02005e;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
